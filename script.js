985e2b7232fd10523b0e97bc7ca3ba48ab3e9bff
Gajanan Konarde (aptlogica) <gajanan.konarde@aptl.local>
2024-10-17 12:58:12 +0530

    partially implemented score logic

--- script.js
+++ script.js
@@ -1,83 +1,39 @@
-const queAnsPairs = [
-  {
-    question: "Which class in Java is used to read data from a file?",
-    options: ["FileReader", "BufferedWriter", "FileWriter", "Scanner"],
-    answer: "FileReader",
-  },
-  {
-    question: "Hashset is a implementation class of ?",
-    options: ["Set", "Map", "List", "Queue"],
-    answer: "Set",
-  },
-  {
-    question: "number of primitive datatypes in Java ?",
-    options: ["8", "6", "10", "7"],
-    answer: "8",
-  },
-  {
-    question: "Can an interface in Java have a method with a body?",
-    options: ["Yes", "No", "Only in abstract class", "Only in enums"],
-    answer: "Yes",
-  },
-  {
-    question: "What is the difference between == and equals() in Java?",
-    options: [
-      "== compares references, equals() compares values",
-      "== compares values, equals() compares references",
-      "== compares data types, equals() compares sizes",
-      "== compares strings, equals() compares objects",
-    ],
-    answer: "== compares references, equals() compares values",
-  },
-  {
-    question: "Runtime exception is Checked or Unchecked exception ?",
-    options: ["Unchecked", "Checked", "Both", "Compile-time"],
-    answer: "Unchecked",
-  },
-  {
-    question: "The underlying structure of LinkedList is ?",
-    options: [
-      "Doubly Linked List",
-      "Array",
-      "Single Linked List",
-      "Hash Table",
-    ],
-    answer: "Doubly Linked List",
-  },
-  {
-    question: "What is the default size of an ArrayList in Java?",
-    options: ["10", "0", "16", "5"],
-    answer: "10",
-  },
-  {
-    question: "String are _____ in nature",
-    options: ["immutable", "mutable", "constant", "variable"],
-    answer: "immutable",
-  },
-  {
-    question: "What is the purpose of the final keyword in Java?",
-    options: [
-      "To restrict modification or inheritance",
-      "To allow method overriding",
-      "To enable runtime polymorphism",
-      "To define abstract methods",
-    ],
-    answer: "To restrict modification or inheritance",
-  },
-];
-
 let n = 5;
 let currentQuestionIndex = 0;
 let score = 0;
-const randomQuestions = getRandomQuestions(queAnsPairs, n);
+let randomQuestions = [];
+const selectedAns = [];
+const isCorrect = [];
 const quizForm = document.getElementById("quizForm");
 
-function loadQuestions() {
+async function loadData() {
+  try {
+    const response = await fetch("quesAnsPairs.json");
+    if (!response.ok) {
+      throw new Error("Error while fetching data", response.status);
+    }
+    const queAnsPairs = await response.json();
+    randomQuestions = getRandomQuestions(queAnsPairs, n);
+    loadQuestions();
+  } catch (error) {
+    console.error("Error fetching questions:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadData();
+});
+
+async function loadQuestions() {
   if (currentQuestionIndex >= randomQuestions.length) {
     displayResult();
     return;
   }
+
   const item = randomQuestions[currentQuestionIndex];
+  if (!item) {
+    console.error("Question not found for index", currentQuestionIndex);
+  }
   displayNumList();
 
   const questionDiv = document.createElement("div");
@@ -85,14 +41,16 @@ function loadQuestions() {
   questionDiv.innerHTML = questionHTML;
 
   item.options.forEach((option) => {
+    const isChecked =
+      selectedAns[currentQuestionIndex] === option ? "checked" : "";
     questionDiv.innerHTML += `
-        <div class="options">
-          <label>
-            <input type="radio" name="q${currentQuestionIndex}" value="${option}">
-            ${option}
-          </label>
-        </div>
-        `;
+    <div class="options">
+    <label>
+    <input type="radio" name="q${currentQuestionIndex}" value="${option}"${isChecked}>
+    ${option}
+    </label>
+    </div>
+    `;
   });
 
   quizForm.appendChild(questionDiv);
@@ -106,11 +64,16 @@ function loadQuestions() {
     );
 
     if (selectedOption) {
-      if (selectedOption.value === item.answer) {
-        score++;
+      selectedAns[currentQuestionIndex] = selectedOption.value;
+      isCorrect[currentQuestionIndex] = selectedOption.value === item.answer;
+      while (currentQuestionIndex < isCorrect.length) {
+        if (isCorrect[currentQuestionIndex] == true) {
+          score++;
+        }
+        currentQuestionIndex++;
       }
-      currentQuestionIndex++;
-
+      console.log(isCorrect, score);
+      // currentQuestionIndex++;
       quizForm.innerHTML = "";
       if (currentQuestionIndex < randomQuestions.length) {
         loadQuestions();
@@ -125,9 +88,14 @@ function loadQuestions() {
   if (currentQuestionIndex !== 0) {
     document.querySelector("#prev-btn").addEventListener("click", (event) => {
       event.preventDefault();
+      const selectedOption = document.querySelector(
+        `input[name="q${currentQuestionIndex}"]`
+      );
       quizForm.innerHTML = "";
       if (currentQuestionIndex > 0) {
+        selectedOption.value = selectedAns[currentQuestionIndex];
         currentQuestionIndex--;
+        isCorrect[currentQuestionIndex] = 0;
         loadQuestions();
       }
     });
@@ -138,14 +106,14 @@ function buttonVisibility() {
   const prevBtn = document.getElementById("prev-btn");
   const nxtBtn = document.getElementById("nxt-btn");
 
-  if (currentQuestionIndex == 0) {
+  if (currentQuestionIndex === 0) {
     prevBtn.style.display = "none";
   } else {
     prevBtn.style.display = "flex";
   }
 
-  if (currentQuestionIndex == randomQuestions.length) {
-    nxtBtn.innerText = "✔";
+  if (currentQuestionIndex === randomQuestions.length) {
+    nxtBtn.innerText = "✓";
     nxtBtn.onclick = (event) => {
       event.preventDefault();
       displayResult(score);
@@ -156,36 +124,32 @@ function buttonVisibility() {
 function displayResult(score) {
   document.querySelector(".container").innerHTML =
     '<div id="result-msg"></div>';
-
   const scorePercent = (score / randomQuestions.length) * 100;
+
   if (scorePercent >= 60) {
     document.getElementById(
       "result-msg"
-    ).innerText = `You succesfully passed the test with ${scorePercent}%`;
+    ).innerText = `You successfully passed the test with ${scorePercent}%`;
   } else {
     document.getElementById(
       "result-msg"
-    ).innerText = `You failed the test. Your score ${scorePercent}`;
+    ).innerText = `You failed the test. Your score is ${scorePercent}%`;
   }
 }
 
 function getRandomQuestions(queAnsPairs, n) {
   const usedIndexes = new Set();
   const result = [];
-  while (result.length < n) {
-    const randomIndex = Math.floor(Math.random() * queAnsPairs.length);
 
+  while (result.length < n) {
+    const randomIndex = Math.floor(
+      Math.random() * queAnsPairs.queAnsPairs.length
+    );
     if (!usedIndexes.has(randomIndex)) {
       usedIndexes.add(randomIndex);
-
-      result.push({
-        question: queAnsPairs[randomIndex].question,
-        options: queAnsPairs[randomIndex].options,
-        answer: queAnsPairs[randomIndex].answer,
-      });
+      result.push(queAnsPairs.queAnsPairs[randomIndex]);
     }
   }
-
   return result;
 }
 
@@ -224,4 +188,4 @@ function displayNumList() {
   });
 }
 
-loadQuestions();
+// loadQuestions();
